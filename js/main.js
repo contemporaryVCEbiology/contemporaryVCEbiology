a9cb2ba3670d433f2e757e27cf3c98763d3bb2f2
iPadisthin <albert@Alberts-MacBook-Pro.local>
2017-12-12 18:16:43 +1100

    videos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var channelId = 'UCtHT4kaRYc0XFXsaxpmDPIg';
+var apiKey = 'AIzaSyAOqpwlI1CYmOBLBVT02GF4VES65IelHko';
+var playlistId = 'PLkY7Yb9m1O2VDBLp6j76W5kzazTMeOPqO';
+
 $(document).ready(function(){
     $('.tabs a').click(function(event){
         event.preventDefault();
@@ -9,4 +13,37 @@ $(document).ready(function(){
             $('#'+tabId).addClass('current');
         }
     });
+    
+    /*$.get(
+        'https://www.googleapis.com/youtube/v3/channels', {
+            part: 'contentDetails',
+            id: channelId,
+            key: apiKey },
+            function(data){
+                $.each(data.items, function(){
+                    console.log(item);
+                })
+            }
+    );*/
+    getVids(playlistId);
+    function getVids(playlistId){
+        $.get(
+            'https://www.googleapis.com/youtube/v3/playlistItems', {
+                part: 'snippet',
+                playlistId: playlistId,
+                key: apiKey },
+                function(data){
+                    var output;
+                    $.each(data.items, function(){
+                        console.log(item);
+                        /*
+                        videoTitle = item.snippet.title;
+                        
+                        output = '<li>'+videoTitle+'</li>';
+                        $('#results').append(output);
+                        */
+                    })
+                }
+        );
+    }
 });
