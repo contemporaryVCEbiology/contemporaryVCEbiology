3e4be07773319599706004bf13b37e97a98dfb2c
iPadisthin <albert.penticoss@gmail.com>
2018-06-14 15:00:07 +1000

    remove "open in new tabs" hover for multimedia videos

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ function displayVids(data){
         videoThumbnail = item.snippet.thumbnails.medium.url;
         videoId = item.snippet.resourceId.videoId;
 
-        output = '<li><a class="no-barba" href="https://youtu.be/'+videoId+'" target="_blank" data-videoId="'+videoId+'"><img src="'+item.snippet.thumbnails.medium.url+'" alt=""><span class="videoTitle">'+videoTitle+'</span>'+ videoDescription+'</a></li>';
+        output = '<li><a class="no-barba" href="https://youtu.be/'+videoId+'" data-videoId="'+videoId+'"><img src="'+item.snippet.thumbnails.medium.url+'" alt=""><span class="videoTitle">'+videoTitle+'</span>'+ videoDescription+'</a></li>';
         //console.log(output);
         $('#results').append(output);
     })
